15a38844373cc8cf7011f3fbec9d84f9352a151d
unknown <ivusony@gmail.com>
2019-01-23 21:47:03 +0100

    easing

--- parallax.js
+++ parallax.js
@@ -1,12 +1,14 @@
 ;
 (function (global) {
+
+    
     function factory(element, ratio){
         if(!ratio){
             ratio = 20
         }
         var duration = 1;
-        var div = element;
-        console.log(div);
+        // var div = element;
+        
 
    
 
@@ -17,40 +19,53 @@
 //         console.log("Screen: " + screen.width + ' : ' + screen.height);
 //    console.log("Window: "+window.innerWidth + ' : ' + window.innerHeight);
 
-        var divPos = div.getBoundingClientRect();
-        var x = divPos.left + (divPos.width/2);
-        var y = divPos.top + (divPos.height/2);
+
+        var elementPos = element.getBoundingClientRect();
+        //calculate new x and y of the element, so that the calculations can be made from the elements center
+        var elementCenterPosition = (function(currentPos){
+            return {
+                x   :   currentPos.left + (currentPos.width/2),
+                y   :   currentPos.top + (currentPos.height/2)
+            }
+        })(elementPos)
+     
 
 
   
-        div.style.transition =  'all 0.3s ease 0s';
+        element.style.cssText =  "-webkit-transition: all 1300ms cubic-bezier(0.000, 0.000, 0.460, 1); -webkit-transition: all 1300ms cubic-bezier(0.000, 0.000, 0.460, 1.195);-moz-transition: all 1300ms cubic-bezier(0.000, 0.000, 0.460, 1.195);-o-transition: all 1300ms cubic-bezier(0.000, 0.000, 0.460, 1.195);transition: all 1300ms cubic-bezier(0.000, 0.000, 0.460, 1.195);transform-origin: cemter;-webkit-backface-visibility: hidden;"
 
         window.addEventListener('mousemove', function(e){
-
-            if(divPos.x>e.clientX){
-                if(divPos.y>e.clientY){
-                        div.style.transform = `translate3d(-${(x - e.clientX)/ratio}px,-${(y - e.clientY)/ratio}px,0)`;
+            //translate3d(X, Y, Z)
+            if(elementPos.x>e.clientX){ //
+                if(elementPos.y>e.clientY){
+                    element.style.transform = `translate3d(-${(elementCenterPosition.x - e.clientX)/ratio}px,-${(elementCenterPosition.y - e.clientY)/ratio}px,0)`;
                 }else{
-                        div.style.transform = `translate3d(-${(x - e.clientX)/ratio}px,${(e.clientY - y)/ratio}px,0)`;
+                    element.style.transform = `translate3d(-${(elementCenterPosition.x - e.clientX)/ratio}px,${(e.clientY - elementCenterPosition.y)/ratio}px,0)`;
                 }
             }else{ //if divX < clientX
-                    if(divPos.y>e.clientY){
-                        div.style.transform = `translate3d(${(e.clientX - x)/ratio}px,-${(y - e.clientY)/ratio}px,0)`;
+                    if(elementPos.y>e.clientY){
+                        element.style.transform = `translate3d(${(e.clientX - elementCenterPosition.x)/ratio}px,-${(elementCenterPosition.y - e.clientY)/ratio}px,0)`;
                     }else{
-                        div.style.transform = `translate3d(${(e.clientX - x)/ratio}px,${(e.clientY - y)/ratio}px,0)`;
+                        element.style.transform = `translate3d(${(e.clientX - elementCenterPosition.x)/ratio}px,${(e.clientY - elementCenterPosition.y)/ratio}px,0)`;
                     }
             }
             
+        })
+
+        element.addEventListener('transitionend' , function(e){
+            console.log('Transition has ended')
         })
 
         //if screen is resized
         
     } //end of factory
 
-    // window.addEventListener('resize', factory(div, ratio))
+    
 
    const parallax = function(elementToBeAnimated, ratio){
-       
+        if(!elementToBeAnimated)return parallax;
+
+        //if string is passed
         if(typeof elementToBeAnimated === 'string'){
             if(!document.querySelector(elementToBeAnimated)){
                 return
@@ -58,7 +73,7 @@
                 return factory(document.querySelector(elementToBeAnimated), ratio);
             }
         }
-
+        //if node is passed
         if(typeof elementToBeAnimated === 'object'){
             return factory(elementToBeAnimated, ratio)
         }
