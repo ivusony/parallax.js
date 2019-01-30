629f2a96c005e727f5b8afb063f165739fc868dd
radulko86 <radulko86@gmail.com>
2019-01-30 16:28:48 +0100

    timer problem, not solved

--- src/timer.js
+++ src/timer.js
@@ -1,8 +1,29 @@
-module.exports = function timer(delay, cb){
-    var lastCall = 0;
+function timer(cb, delay, totalDistance){
+    //seting the starting time in miliseconds
+    var startTime = Date.now();
+    //total distance allready passed
+    var distancePassed = 0;
+    //step is calculated as the result of the division of the desired location and time 
+    //to finish the animation in miliseconds
+    var step = totalDistance/1000;
 
     function inner(){
-
+        //set stop to true if true the passed distandce is greather or equal to desired location
+        var stop = distancePassed<totalDistance ? false : true;
+        //if true, return
+        if(stop)return;
+        
+        setTimeout(function(){
+            distancePassed+=step;
+            var passed = Date.now() - startTime;
+            cb(passed, inner);
+        },delay)
     }
-    var now = (new Date).getTime();
+    inner()
 }
+
+
+timer(function(pass, cb){
+    console.log('Delayed by ' + pass + ' miliseconds');
+    cb();
+}, 8, 1000)
